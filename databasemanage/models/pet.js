08bced094d646cf8ab43087762e50f9cdc77747e
AnandaMontoly <amontoly@smith.edu>
2020-11-07 16:35:46 -0500

    designing models

--- databasemanage/models/pet.js
+++ databasemanage/models/pet.js
@@ -3,20 +3,20 @@ const sequelize = require("../databaseconnection.js")
 //https://www.geeksforgeeks.org/node-js-mysql-create-table-using-sequelize/?ref=rp
 //https://dev.to/darshanbib/user-management-for-node-js-mysql-using-sequelize-and-passportjs-44kj
 
-const User = sequelize.define("user", {
-  user_id:{
+const Pet = sequelize.define("pet", {
+  pet_id:{
     type:Sequelize.INTEGER,
     autoIncrement:true,
     allowNull:false
     primaryKey:true
   },
   name: {type: Sequelize.STRING},
-  email: {type: Sequelize.STRING},
+  happiness: {type: Sequelize.INTEGER},
+  hunger: {type: Sequelize.INTEGER},
+  health: {type: Sequelize.INTEGER},
+  personality: {type:Sequelize.STRING},
   myDate: {type: Sequelize.DATE,
             defaultValue: Sequelize.NOW },
-  //MAKE SURE TO HASH PASSWORD: ANANDA
-  password: {type: Sequelize.STRING},
-  createdAt: Sequelize.DATE,
-  updatedAt: Sequelize.DATE
+  createdAt:Sequelize.DATE
 })
-module.experts = User;
+module.experts = Pet;
