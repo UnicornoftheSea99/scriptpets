05cef1fff1e594e8e29c5f1152543f6d0982adf2
AnandaMontoly <amontoly@smith.edu>
2020-11-07 16:07:57 -0500

    database connection. lmk if you want certs

--- accounts/handleaccounts.js
+++ accounts/handleaccounts.js
@@ -7,6 +7,16 @@ var sequelize = new Sequelize('scriptpets', 'maxroach', ''{
   port: 26257,
   logging: false,
   dialectOptions: {
-
-  }
+    ssl: {
+               ca: fs.readFileSync('certs/ca.crt')
+                   .toString(),
+               key: fs.readFileSync('certs/client.maxroach.key')
+                   .toString(),
+               cert: fs.readFileSync('certs/client.maxroach.crt')
+                   .toString()
+           }//be very careful with this part. do not share CRTS. it will make our database insecure :(
+      }
 });
+
+//this is our database connection
+module.exports = sequelize
