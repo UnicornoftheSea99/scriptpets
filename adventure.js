f7a461c7713e559b150af0773acf2edd479a3e3b
UnicornoftheSea99 <erhyu@smith.edu>
2020-11-07 22:59:18 -0800

    pretty much done with story

--- adventure.js
+++ adventure.js
@@ -125,7 +125,11 @@ const textNodes = [
     id: 4,
     text: 'You have run home scared and so will return penniless.',
     options: [
-      {
+        {
+            text: 'End Game',
+            nextText: 18
+        },
+        {
         text: 'Restart',
         nextText: -1
       }
@@ -133,10 +137,14 @@ const textNodes = [
   },
   {
     id: 5,
-    text: 'You have died honorably in battle. You have earned +10 coins.',
+    text: 'You have died honorably in battle. You have earned 10 coins.',
     bank= earningMoney(10),
     options: [
-      {
+        {
+            text: 'End Game',
+            nextText: 18
+        },
+        {
         text: 'Restart',
         nextText: -1
       }
@@ -145,7 +153,7 @@ const textNodes = [
   {
     id: 6,
     text: 'You pull out a sword and charge at the troll. You succeed in slaying it. You are rewarded 10 coins.',
-    bank= earningMoney(15),
+    bank= earningMoney(10),
     options: [
       {
         text: 'Accept the coins and continue on your way.',
@@ -160,10 +168,10 @@ const textNodes = [
   {
     id: 7,
     text: 'You manage to distract the troll, but are injured in the process. You are rewarded 5 coins.',
+    bank= earningMoney(5),
     options: [
       {
         text: 'Accept the coins and continue on your way.',
-        bank= earningMoney(5),
         nextText: 12
       },
       {
@@ -172,46 +180,149 @@ const textNodes = [
       },
     ]
   },
-//   {
-//     id: 8,
-//     text: 'Your attempts to run are in vain and the monster easily catches.',
-//     options: [
-//       {
-//         text: 'Restart',
-//         nextText: -1
-//       }
-//     ]
-//   },
-//   {
-//     id: 9,
-//     text: 'You foolishly thought this monster could be slain with a single sword.',
-//     options: [
-//       {
-//         text: 'Restart',
-//         nextText: -1
-//       }
-//     ]
-//   },
-//   {
-//     id: 10,
-//     text: 'The monster laughed as you hid behind your shield and ate you.',
-//     options: [
-//       {
-//         text: 'Restart',
-//         nextText: -1
-//       }
-//     ]
-//   },
-//   {
-//     id: 11,
-//     text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
-//     options: [
-//       {
-//         text: 'Congratulations. Play Again.',
-//         nextText: -1
-//       }
-//     ]
-  //}
+  {
+    id: 8,
+    text: 'You succeed in reasoning with the troll. He shakes your hand and gives you 15 coins for a scintillating conversation.',
+    bank= earningMoney(15),
+    options: [
+        {
+            text: 'Accept the coins and continue on your way.',
+            nextText: 12
+          },
+          {
+            text: 'Go home with what coins you have earned.',
+            nextText: 13
+          },
+    ]
+  },
+  {
+    id: 9,
+    text: 'The troll does not agree with your company and decided to eat you. You die instantly but earn 5 coins.',
+    bank= earningMoney(15),
+    options: [
+        {
+            text: 'End Game',
+            nextText: 18
+        },
+        {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 10,
+    text: 'You walk towards the ocean and the fearsome sea monster the Unagi rises from the water. Do you:',
+    options: [
+      {
+        text: 'Stay to fight the Unagi',
+        nextText: (function(num){
+            if(num ==0){return 14;} 
+            else if (num==1){return 5;} 
+            else if (num==2){return 5;} randNumber()})
+      },
+      {
+        text: 'Run away from the water towards the town.',
+        nextText: 11
+      },
+      {
+        text: 'Run away back home',
+        setState: { brave: false},
+        nextText: 4
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'You walk towards the town and come across the locals, who are not fond of outsiders. They ask you to fight the Unagi.',
+    options: [
+        {
+            text: 'You agree.',
+            nextText: (function(num){
+                if(num ==0){return 14;} 
+                else if (num==1){return 5;} 
+                else if (num==2){return 5;} randNumber()})
+        },
+        {
+            text: 'You refuse and are chased out of town. You are then caught in a tsunami and die.',
+            nextText: 18
+        }
+
+    ]
+  },
+  {
+    id: 12,
+    text: 'You continue on and find a myserious box lying in the road. Do you:',
+    options: [
+       {
+        text: 'Open it.',
+        nextText: 15
+      },
+      {
+        text: 'Leave it alone and continue on.',
+        nextText: 16
+      }
+    ]
+  },
+  {
+    id: 13,
+    text: 'You head home after a fruitful adventure.',
+    options: [
+        {
+            text: 'End Game',
+            nextText: 18
+        },
+        {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 14,
+    text: 'You defeat the Unagi and the locals, who had been terrorized by the Unagi for years, thank you with 20 coins.',
+    bank= earningMoney(20),
+    options: [
+        {
+            text: 'Accept the coins and continue on your way.',
+            nextText: 12
+          },
+          {
+            text: 'Go home with what coins you have earned.',
+            nextText: 13
+          },
+    ]
+  },
+  {
+    id: 15,
+    text: 'The box contains lost Aztec gold. You get 40 coins. Your adventure has come to a close',
+    bank= earningMoney(20),
+    options: [
+        {
+            text: 'Finish Adventure.',
+            nextText: 17
+          },
+    ]
+  },
+  {
+    id: 16,
+    text: 'You leave the box alone and successfully reach your destination. This adventure has come to a close.',
+    options: [
+        {
+            text: 'Finish Adventure.',
+            nextText: 17
+          },
+    ]
+  },
+  {
+    id: 17,
+    text: 'Congratulations on completing all the challenges in your path. Here are 10 bonus coins.',
+    bank= earningMoney(10),
+  },
+  {
+    id: 18,
+    text: 'Your adventure ends here.',
+  }
 ]
 
 startAdventure()
